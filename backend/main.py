03c295bef2738f95ba44b7ec209cf84ab45d0659
kalebda <kalebu246@gmail.com>
2022-08-21 20:05:43 +0300

    remove unwanted tracked files

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+import os
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware, db
+from model import PhoneBook
+from schema import PhoneBook as SchemaPhoneBook
+
+load_dotenv(".env")
+
+app = FastAPI()
+
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+
+
+@app.get("/")
+async def get_phone_number():
+    data = db.session.query(PhoneBook).order_by(PhoneBook.lastname.asc())
+    return {"data": data}
+
+
+@app.post("/add-phoneNumber/", response_model=SchemaPhoneBook)
+def add_phone_number(_phoneNumber: SchemaPhoneBook):
+    db_phoneBook = PhoneBook(firstname=_phoneNumber.firstname, lastname=_phoneNumber.lastname, phonenumber=_phoneNumber.phonenumber)
+    db.session.add(db_phoneBook)
+    db.session.commit()
+    return db_phoneBook
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
